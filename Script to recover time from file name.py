4c923ab899c022d10bd61b127b8087332f5bc55f
_SUBARU_3776_ <71259928+SUBARU3776@users.noreply.github.com>
2023-05-10 16:35:53 +0900

    Update Script to recover time from file name.py

--- Script to recover time from file name.py
+++ Script to recover time from file name.py
@@ -1,7 +1,8 @@
 import base64
+import time
 
 # 画像ファイル名
-s_filename = 'hogehoge'
+s_filename = ''
 
 # デコード済みファイル名
 s_filename += '=' * (-len(s_filename) % 4)  # パディング文字を追加
@@ -10,6 +11,6 @@ i_filename = int.from_bytes(base64.urlsafe_b64decode(s_filename.replace('-', '+'
 # 日時
 time_ms = (i_filename >> 46) + 1288834974657
 time_sec = time_ms / 1000.0
-date_str = time.strftime('%Y-%m-%d %H:%M:%S %z', time.localtime(time_sec))
+date_str = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(time_sec + time.timezone + 9 * 3600))
 
 print(date_str)
